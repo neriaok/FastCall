12bffc328e500800664cd8204e0ddb30b2ccf0cc
neriaok <neriaokabik.com>
2025-03-26 18:07:09 +0200

    with design

--- frontend-react/src/pages/FastCall.jsx
+++ frontend-react/src/pages/FastCall.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 export function FastCall() {
-    const handleCall = () => {
-        window.location.href = 'tel:+0529541254';
+    const handleCall = (number) => {
+        window.location.href = `tel:+972${number}`;
     };
 
     return (
         <section>
             <h1>FastCall</h1>
-            <button onClick={handleCall}>Call</button> 
+            <button onClick={() => handleCall('0529541254')}>Call 1</button> 
+            <button onClick={() => handleCall('0524185516')}>Call 2</button> 
+            <button onClick={() => handleCall('0506797941')}>Call 3</button> 
         </section>
     );
 }
